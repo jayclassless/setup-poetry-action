da6c1bb0b16a7196b0ebd2da6c4eb3071a338ff7
Jason Simeone <jay@classless.net>
2020-08-29 20:59:27 -0400

    First try

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const exec = require('@actions/exec');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
 
 
-// most @actions toolkit packages have async methods
+function is_yes(value) {
+  return ['yes', 'true'].includes(value.toLowerCase());
+}
+
+
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const version = core.getInput('version');
+    const preview = is_yes(core.getInput('preview'));
+    const createVirtualenvs = is_yes(core.getInput('create_virtualenvs'));
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    core.info('Retrieving Poetry Installation Script...');
+    await exec ('curl -O -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py');
 
-    core.setOutput('time', new Date().toTimeString());
+    core.info('Installing Poetry...');
+    const flags = preview ? '--preview' : version ? `--version=${version}` : '';
+    await exec.exec(`python get-poetry.py --yes ${flags}`);
+
+    core.info('Configuring environment...');
+    core.addPath(path.join(os.homedir(), '.poetry', 'bin'));
+    core.info(`PATH SET TO: ${process.env['PATH']}`);
+    if (!createVirtualenvs) {
+      await exec.exec('poetry config virtualenvs.create false');
+    }
+
+    core.info('Cleaning up...');
+    fs.unlinkSync('get-poetry.py');
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
+
 run();
+
